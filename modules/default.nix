6cd420d7afde838bd9f38400f7a41c6ddddcc6bf
xeals <dev@xeal.me>
2020-10-17 11:59:01 +1100

    services: port amdgpu services from private config

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,7 @@
 {
-  # Add your NixOS modules here
-  #
-  # my-module = ./my-module;
+  amdgpu-common = ./services/hardware/amdgpu-common.nix;
+  amdgpu-fan = ./services/hardware/amdgpu-fan.nix;
+  amdgpu-pwm = ./services/hardware/amdgpu-pwm.nix;
+  radeon-profile-daemon = ./services/hardware/radeon-profile-daemon.nix;
 }
 
