7959b877d9c3006eccf106d725d67072ba96dcd5
xeals <dev@xeal.me>
2020-10-18 17:47:48 +1100

    jetbrains: rework following the Emacs builder
    This commit moves a lot of the heavy lifting out of callPackage and back
    into regular import, following conventions from upstream Nixpkgs. It
    allows for a clearer and less magic definition of new packages,
    particularly by downstream, as well, by exposing the *Build package
    callers in the attribute set.

--- pkgs/applications/editors/jetbrains/wrapper.nix
+++ pkgs/applications/editors/jetbrains/wrapper.nix
@@ -12,22 +12,19 @@ let
     then pluginsFun self
     else pluginsFun;
 
+  # FIXME: Is this still needed?
   info = builtins.parseDrvName package.name;
-
   badPlugins = filter (p: ! elem info.name p.jetbrainsPlatforms) plugins;
   errorMsg = "plugins [ ${toString (map (p: p.name) badPlugins)} ] are not available for platform ${info.name}";
 in
 
 assert assertMsg (length badPlugins == 0) errorMsg;
 
-package.overrideAttrs (oldAttrs: {
-  # FIXME: versioning: could just expose upstream
-  name = "${info.name}-with-plugins-${info.version}";
-
+appendToName "with-plugins" (package.overrideAttrs (oldAttrs: {
   inherit plugins;
   installPhase = oldAttrs.installPhase + ''
     for plugin in $plugins; do
       ln -s "$plugin" "$out/$name/plugins/$(basename $plugin)"
     done
   '';
-})
+}))
