d36de346ade4721cb5b944f49fef4ec33ea427f0
xeals <dev@xeal.me>
2023-03-06 12:07:16 +1100

    packages: convert path interpolation to concat
    I have no idea what the fuck is happening here but interpolating the
    path leads to the whole result being converted to a (-n invalid) store
    path, but concat doesn't. The former results in realisation failing
    running under `nix-build`, i.e., NUR updates.

--- callUnitRoot.nix
+++ callUnitRoot.nix
@@ -7,9 +7,9 @@
 let
   shards = lib.attrNames (builtins.readDir root);
   namesForShard = shard: lib.mapAttrs'
-    (name: _: { inherit name; value = "${root}/${shard}/${name}"; })
-    (builtins.readDir "${root}/${shard}");
+    (name: _: { inherit name; value = root + "/${shard}/${name}"; })
+    (builtins.readDir (root + "/${shard}"));
   namesToPath = lib.foldl' lib.recursiveUpdate { } (map namesForShard shards);
-  units = lib.mapAttrs (_: path: pkgs.callPackage "${path}/${packageFun}" { }) namesToPath;
+  units = lib.mapAttrs (_: path: pkgs.callPackage (path + "/${packageFun}") { }) namesToPath;
 in
 units
